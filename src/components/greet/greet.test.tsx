558364c4c4df19884cc6624bc5a25caa2e61d9c1
chandan <chandan.uidevops@gmail.com>
2023-03-07 01:01:00 +0530

    third commit

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,13 +1,20 @@
 import { render,screen } from "@testing-library/react";
 import { Greet } from "./greet";
-test('Greet render correctly',()=>{
-    render(<Greet />)
-    const textElement = screen.getByText(/hello/i)
-    expect(textElement).toBeInTheDocument()
+
+describe('Greet',()=>{
+    test('Greet render correctly',()=>{
+        render(<Greet />)
+        const textElement = screen.getByText(/hello/i)
+        expect(textElement).toBeInTheDocument()
+    })
+    // describe('Nested',()=>{
+    //     test('Greet renders with a name',()=>{
+    //         render(<Greet name="Chandan" />)
+    //         const textElement = screen.getByText(/hello Chandan/i)
+    //         expect(textElement).toBeInTheDocument()
+    //     })
+    // })
+ 
 })
 
-test('Greet renders with a name',()=>{
-    render(<Greet name="Chandan" />)
-    const textElement = screen.getByText(/hello Chandan/i)
-    expect(textElement).toBeInTheDocument()
-})
+
--- src/components/greet/greet.tsx
+++ src/components/greet/greet.tsx
@@ -1,6 +1,4 @@
-type GreetProps={
-    name?:string
-}
+import { GreetProps } from "./greet.types"
 export const Greet=(props:GreetProps)=>{
-    return (<div>Hello {props.name}</div>)
+    return (<div>Hello {props.name?props.name:'Guest'}</div>)
 }
